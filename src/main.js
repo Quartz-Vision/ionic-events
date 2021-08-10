2fbeba779027b720b17ce7d079cba60f784f8797
Lia Va <mistickusya.2012@gmail.com>
2021-08-10 22:19:25 +0300

    fix: add d.ts files

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import VueEventsHelper from '@quartz-vision/vue-events-helper';
 const quartz = new VueEventsHelper('quartz');
 
 import longTap from './long-tap.js';
-quartz.register('long-tap', longTap);
+quartz.registerEvent('long-tap', longTap);
 
 
 export default quartz;
